0c7cbac75a0fbaf4223abcd315172dbedd976ea8
Éderson T. Szlachta <edersonszlachta@gmail.com>
2014-11-13 09:09:28 -0200

    Avoid conflict with Window Title.

--- bin/open-terminal.sh
+++ bin/open-terminal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# -*- coding: utf-8; -*-
-#
+
+
 # Copyright (C) 2014 Éderson T. Szlachta <edersonszlachta@gmail.com>
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -15,22 +15,22 @@
 # Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 
-#
-# Basic Idea
+
+# Where the Idea came from
 # http://www.linuxquestions.org/questions/linux-software-2/shell-script-to-start-another-program-if-not-running-625589/#post3077532
-#
+
 # Tip to use `xwininfo` to find the window
 # http://www.linuxquestions.org/questions/linux-general-1/how-to-bring-up-application-window-to-front-from-shell-script-83545/
-#
+
 # Command that do the Magic while calling this script
 # http://askubuntu.com/questions/21262/shell-command-to-bring-a-program-window-in-front-of-another#21274
-#
+
 # wmctrl
 # http://tomas.styblo.name/wmctrl/
 # http://spiralofhope.com/wmctrl-examples.html
 # http://www.freedesktop.org/wiki/Software/wmctrl/
-#
-#
+
+# xdotool
 # http://superuser.com/questions/382616/detecting-currently-active-window#615946
 
 
@@ -38,14 +38,19 @@
 # TEMP file to save Current Window ID
 TMP_FILE='/tmp/.last_window_id'
 
+# Create TEMP File
+touch $TMP_FILE
+
 # Search terminal PID
-PID=`ps -ef | grep -v grep | grep gnome-terminal | awk '{print $2}'`
+PID=`ps -e | grep -v grep | grep gnome-terminal | awk '{print $1}'`
 
 # get current Window Name to avoid PID conflict
 CURRENT_PID=`xdotool getwindowfocus getwindowname`
 
 # get current Window ID by Window Name
 CURRENT_ID=`wmctrl -l -p | grep "$CURRENT_PID" | awk '{print $1}'`
+
+
 
 
 if test "$PID" = ""
